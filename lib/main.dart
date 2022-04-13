d5aac654ee5d1187a386db519e91d65b4320021b
steve <steve.harris250105@gmail.com>
2022-04-14 00:16:43 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:technokingdiesel/widget.dart';
 
+const page = PageTransitionsTheme(builders: {
+  TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+  TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+  TargetPlatform.windows: CupertinoPageTransitionsBuilder(),
+  TargetPlatform.macOS: CupertinoPageTransitionsBuilder(),
+  TargetPlatform.linux: CupertinoPageTransitionsBuilder(),
+});
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
@@ -27,9 +34,7 @@ class _MyAppState extends State<MyApp> {
     initialization();
   }
 
-  void initialization() async {
-    await Future.delayed(const Duration(seconds: 1));
-
+  void initialization() {
     FlutterNativeSplash.remove();
   }
 
@@ -38,15 +43,42 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+          pageTransitionsTheme: page,
+          inputDecorationTheme: InputDecorationTheme(
+            fillColor: Colors.white,
+            contentPadding: EdgeInsets.all(1),
+            filled: true,
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: Color.fromARGB(255, 0, 80, 145),
+                width: 2,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: Color.fromARGB(255, 0, 80, 145),
+                width: 4,
+              ),
+            ),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            suffixStyle: TextStyle(color: Colors.white),
+          ),
           fontFamily: 'Nunito',
           appBarTheme: AppBarTheme(
-            titleTextStyle: TextStyle(fontFamily: 'Nunito',fontWeight: FontWeight.w700,fontSize: 20),
+            titleTextStyle: TextStyle(
+                fontFamily: 'Nunito',
+                fontWeight: FontWeight.w700,
+                fontSize: 20),
             backgroundColor: Color(0xFF4080B4),
           ),
           floatingActionButtonTheme: FloatingActionButtonThemeData(
-            backgroundColor: Color(0xFF4080B4),
+            backgroundColor: Color.fromARGB(255, 0, 80, 145),
           ),
-          primaryColor: Color(0xFF4080B4),
+          primaryColor: Colors.red,
+          colorScheme: ColorScheme.fromSwatch(accentColor: Colors.green),
           scaffoldBackgroundColor: Colors.grey.shade200
 
           //
@@ -68,63 +100,95 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
-  // void _incrementCounter() async {
-  //   await FirebaseFirestore.instance
-  //       .collection('data')
-  //       .doc((_counter).toString())
-  //       .set({'num': _counter});
-
-  // }
+  TextEditingController? _addNameController;
+  String? searchString = '';
+  var focusNode = FocusNode();
+  @override
+  void initState() {
+    super.initState();
+    _addNameController = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext context) {
     log('scafold');
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
-          title: Text(widget.title),
+          title: Container(
+            width: MediaQuery.of(context).size.width,
+            child: TextFormField(
+              cursorColor: Color.fromARGB(255, 0, 80, 145),
+              decoration: InputDecoration(
+                hintText: 'Cari Mesin...',
+                prefixIcon: Icon(Icons.search),
+              ),
+              focusNode: focusNode,
+              onFieldSubmitted: (value) {
+                focusNode.unfocus();
+              },
+              keyboardType: TextInputType.name,
+              controller: _addNameController,
+              onChanged: (val) {
+                setState(() {
+                  searchString = val;
+                });
+              },
+            ),
+          ),
         ),
         body: Center(
-            child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                stream: FirebaseFirestore.instance
-                    .collection('data')
-                    .orderBy('num')
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  return snapshot.connectionState == ConnectionState.waiting
-                      ? Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      : ListView.builder(
-                          shrinkWrap: true,
-                          reverse: true,
-                          itemCount: snapshot.data!.docs.length,
-                          itemBuilder: (context, index) {
-                            _counter = snapshot.data!.docs.length;
-                            log('rebuild');
-                            return InkWell(
+          child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream: FirebaseFirestore.instance
+                  .collection('data')
+                  .orderBy('seri')
+                  .limit(50)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                return snapshot.connectionState == ConnectionState.waiting
+                    ? Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Color(0xFF4080B4),
+                          color: Color.fromARGB(255, 0, 80, 145),
+                        ),
+                      )
+                    : ListView.builder(
+                        shrinkWrap: true,
+                        reverse: true,
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          _counter = snapshot.data!.docs.length;
+                          log('rebuild');
+                          return Card(
+                            child: InkWell(
                               onTap: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => Detail(
-                                      '',
-                                      snapshot.data!.docs.reversed
-                                          .toList()[index]
-                                          .data()),
-                                ));
+                                focusNode.unfocus();
+                                Navigator.of(context)
+                                    .push(MaterialPageRoute(builder: (context) {
+                                  var data = snapshot.data!.docs.reversed
+                                      .toList()[index]
+                                      .data();
+                                  data['id'] = snapshot.data!.docs.reversed
+                                      .toList()[index]
+                                      .id;
+                                  return Detail('', data);
+                                }));
                               },
                               child: Text(
                                 snapshot.data!.docs.reversed
                                     .toList()[index]
-                                    .data()['num']
+                                    .data()['seri']
                                     .toString(),
-                                style: TextStyle(fontSize: 20),
+                                style: TextStyle(fontSize: 25),
                               ),
-                            );
-                          },
-                        );
-                })),
+                            ),
+                          );
+                        },
+                      );
+              }),
+        ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
+          onPressed: () async {
             Navigator.of(context).push(MaterialPageRoute(
               builder: (context) => Detail('Add Document'),
             ));
--- lib/widget.dart
+++ lib/widget.dart
@@ -1,5 +1,6 @@
-import 'dart:math';
+import 'dart:developer';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 enum Status {
@@ -18,6 +19,7 @@ class Detail extends StatefulWidget {
 }
 
 class _DetailState extends State<Detail> {
+  String? seri;
   late final Status status;
   @override
   void initState() {
@@ -33,20 +35,62 @@ class _DetailState extends State<Detail> {
 
   String appTitle() {
     if (status == Status.view) {
-      return widget.data!['num'].toString();
+      return widget.data!['seri'].toString();
     } else {
       return '' + widget.which;
     }
+  }
+
+  void delete() async {
+    ScaffoldMessenger.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(SnackBar(
+        padding: EdgeInsets.zero,
+        duration: Duration(minutes: 1),
+        content: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: const Text(
+                'Apakah Anda yakin ?',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Spacer(),
+            TextButton(
+                onPressed: () async {
+                  await FirebaseFirestore.instance
+                      .collection('data')
+                      .doc(widget.data!['id'])
+                      .delete();
+                         ScaffoldMessenger.of(context).removeCurrentSnackBar();
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Delete',
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.w700))),
+            TextButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                },
+                child: const Text('Cancle')),
+          ],
+        ),
+        backgroundColor: Color.fromARGB(255, 77, 76, 76),
+      ));
   }
 
   Widget value() => Container(
         alignment: Alignment.center,
         padding: const EdgeInsets.all(6),
         child: TextFormField(
-          onChanged: (v) {},
+          onChanged: (v) {
+            seri = v;
+          },
           readOnly: status == Status.view,
           initialValue:
-              status != Status.add ? widget.data!['num'].toString() : '',
+              status != Status.add ? widget.data!['seri'].toString() : '',
           decoration: InputDecoration(
             border:
                 status != Status.view ? OutlineInputBorder() : InputBorder.none,
@@ -60,25 +104,55 @@ class _DetailState extends State<Detail> {
         resizeToAvoidBottomInset: false,
         floatingActionButton: (status == Status.edit || status == Status.add)
             ? FloatingActionButton(
-                onPressed: () {}, child: (Icon(Icons.done_outline_rounded)))
+                onPressed: () async {
+                  if (status == Status.add) {
+                    await FirebaseFirestore.instance
+                        .collection('data')
+                        .doc( DateTime.now().toIso8601String())
+                        .set({'seri': seri});
+                         Navigator.of(context).pop();
+                  } else {
+                    await FirebaseFirestore.instance
+                        .collection('data')
+                        .doc(widget.data!['id'])
+                        .update({
+                      'seri': seri,
+                      'date': DateTime.now().toIso8601String()
+                    });
+                    Navigator.of(context).pop();
+                     Navigator.of(context).pop();
+                  }
+                },
+                child: (Icon(Icons.done_outline_rounded)))
             : SizedBox(),
-        appBar: AppBar(title: Text(appTitle()), actions: [
-          status == Status.view
-              ? IconButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => Detail('Edit', widget.data),
-                    ));
-                  },
-                  icon: Icon(Icons.edit))
-              : SizedBox(), status == Status.view
-              ? IconButton(
-                  onPressed: () {
-                   
-                  },
-                  icon: Icon(Icons.delete))
-              : SizedBox()
-        ]),
+        appBar: AppBar(
+            leading: IconButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context)..removeCurrentSnackBar();
+
+                  Navigator.of(context).pop();
+                },
+                icon: Icon(Icons.arrow_circle_left_rounded)),
+            automaticallyImplyLeading: false,
+            title: Text(appTitle()),
+            actions: [
+              status == Status.view
+                  ? IconButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => Detail('Edit', widget.data),
+                        ));
+                      },
+                      icon: Icon(Icons.edit))
+                  : SizedBox(),
+              status == Status.view
+                  ? IconButton(
+                      onPressed: () {
+                        delete();
+                      },
+                      icon: Icon(Icons.delete))
+                  : SizedBox()
+            ]),
         body: value());
   }
 }
